5c72e7f54a61c744aec51e2a5d770b4c844182ed
ne-aki <geunhyeong930@gmail.com>
2026-09-06 21:23:52 +0900

    gitignore 추가

--- board.sql
+++ board.sql
@@ -1,4 +1,4 @@
-CREATE TABLE TEST_BOARD (
+CREATE TABLE board_study.TEST_BOARD (
 	BOARD_NUM INT PRIMARY KEY AUTO_INCREMENT
 	, TITLE VARCHAR(100) NOT NULL
 	, CONTENT TEXT
@@ -10,3 +10,9 @@ select * from test_board;
 select * from information_schema.ALL_PLUGINS ap ;
 insert into test_board (TITLE, CONTENT, WRITER) values ('제목', '내용', '작성자');
 select * from board_study.test_board tb ;
+SELECT
+    BOARD_NUM
+    , TITLE
+    , WRITER
+    , REG_DATE
+FROM board_study.TEST_BOARD;
